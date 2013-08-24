e93e778bd914aa62b9b4b42e9030f6102d4283fd
abhimanyuma <abhimanyu.m.a@gmail.com>
2013-08-24 11:09:25 +0530

    Change Routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 FrontpageNews::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/humans"
+
+  match '/help',    to: 'static_pages#help',     via: 'get'
+  match '/about',   to: 'static_pages#humans',   via: 'get'
+  match '/contact', to: 'static_pages#contact',  via: 'get'
+
+  match '/', to: 'static_pages#home', via: 'get'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
