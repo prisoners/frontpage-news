872813bf89cd1ede71d7a76483f9c7c0221ca1cb
abhimanyuma <abhimanyu.m.a@gmail.com>
2013-08-24 12:54:20 +0530

    Add named routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 FrontpageNews::Application.routes.draw do
 
+  match '/', to: 'static_pages#home', via: 'get'
+  
   match '/help',    to: 'static_pages#help',     via: 'get'
   match '/about',   to: 'static_pages#humans',   via: 'get'
   match '/contact', to: 'static_pages#contact',  via: 'get'
-
-  match '/', to: 'static_pages#home', via: 'get'
+  match '/signup',  to: 'users#new',             via: 'get'
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
