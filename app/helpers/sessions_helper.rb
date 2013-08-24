89685d9c6797a4f37676c46eca26961bb24212cf
abhimanyuma <abhimanyu.m.a@gmail.com>
2013-08-24 21:33:15 +0530

    Add sign out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,7 +9,7 @@ module SessionsHelper
   def signed_in?
     !current_user.nil?
   end
-  
+
   def current_user=(user)
     @current_user = user
   end
@@ -17,5 +17,10 @@ module SessionsHelper
   def current_user
     remember_token = User.encrypt(cookies[:remember_token])
     @current_user ||= User.find_by(remember_token: remember_token)
+  end
+
+  def sign_out
+    self.current_user = nil
+    cookies.delete(:remember_token)
   end
 end
