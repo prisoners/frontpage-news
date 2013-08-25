336d2338f9ab0ccd6b542438d5459d73744ee5d0
abhimanyuma <abhimanyu.m.a@gmail.com>
2013-08-25 14:42:39 +0530

    Change content from string to text

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130825053350) do
+ActiveRecord::Schema.define(version: 20130825091125) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
-    t.string   "content"
+    t.text     "content",    limit: 255
     t.string   "author"
     t.date     "date"
     t.datetime "created_at"
